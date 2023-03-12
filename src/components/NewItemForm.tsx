c021101584e2f143c7bf930756e761cbc3352c7f
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-03-12 13:52:43 +0100

    Added Submit on enter

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -10,11 +10,20 @@ type NewItemFormProps = {
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
+  const handleAddText: React.KeyboardEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
   return (
     <NewItemFormContainer>
       <NewItemInput
         value={text}
         ref={inputRef}
+        onKeyPress={handleAddText}
         onChange={(e) => setText(e.target.value)}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
